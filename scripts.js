2776a29593f40c9dcd423a021ce8f4a07e2944fb
kaluahunter <priyanshugupta2407@gmail.com>
2025-04-04 12:17:36 +0530

    Final commit: added local storage and complete feature

--- scripts.js
+++ scripts.js
@@ -4,38 +4,56 @@ document.addEventListener("DOMContentLoaded",()=>{
     const lst=document.getElementById("todo-list");
     document.getElementById("");
     let mp= new Map();
+    render();
+    
 
 lst.addEventListener('click',(event)=>{
     var g=event.target;
     if(g.tagName=='LI')
+    {
         g.classList.toggle("completed");
+        x=mp.get(Number(g.getAttribute('id'))).completed;
+        mp.get(Number(g.getAttribute('id'))).completed=!x;
+        localStorage.setItem("task",JSON.stringify([...mp]));
+    }
     else if(g.tagName=='BUTTON')
     {
         var li = g.parentElement;
         var key = li.getAttribute('id');
         li.remove();
         mp.delete(Number(key));
+        localStorage.setItem("task",JSON.stringify([...mp]));
         console.log(mp);
     }
     else
     return;
-});8
+});
 add.addEventListener('click',()=>{
     var text=title.value.trim();
     if(text=="")
         return;
     title.value="";
-    mp.set(Date.now(),text);
-    render(mp);
+    let value={
+        title:text,
+        completed:false
+    };
+    mp.set(Date.now(),value);
+    localStorage.setItem("task",JSON.stringify([...mp]));
+    console.log(mp);
+    render();
 });
 
-function render(mp){
+function render(){
     lst.innerHTML="";
     var g=document.createElement('ul');
+    let stored=localStorage.getItem("task");
+    mp=new Map(JSON.parse(stored));
     mp.forEach((value,key) => {
         var li=document.createElement('li');
-        li.innerHTML=`<span> ${value}</span> <button> delete </button>`;
+        li.innerHTML=`<span> ${value.title}</span> <button> delete </button>`;
         li.setAttribute('id',key);
+        if(value.completed==true)
+            li.classList.add("completed");
         lst.appendChild(li);
     }); 
 }
